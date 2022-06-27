e77a0b5c3ba7c7026edd25d7eac8d433793294f2
Thales Cezar Castro <thalesccastro@gmail.com>
2022-06-27 12:32:55 -0300

    feat: app index

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+if (!array_key_exists('PATH_INFO', $_SERVER)) {
+    notFound();
+}
+
+$allowed_methods = ['GET', 'POST', 'PUT', 'DELETE'];
+$methods_with_id = ['PUT', 'DELETE'];
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ((!in_array($method, $allowed_methods) ||
+    (in_array($method, $methods_with_id) && !array_key_exists('id', $_GET)))) {
+    notFound();
+}
+
+$config_path = __DIR__ . '/src/Config/';
+require_once $config_path . 'autoload.php';
+
+$resource = $_SERVER['PATH_INFO'];
+$routes = require_once $config_path . 'routes.php';
+
+if (!array_key_exists($resource, $routes)) {
+    notFound();
+}
+
+$controller = new $routes[$resource];
+
+$response = false;
+
+switch($method) {
+    case 'GET':
+        if (!array_key_exists('id', $_GET)) {
+            $response = $controller->index();
+        } else {
+            $response = $controller->show($_GET['id']);
+        }
+        break;
+    case 'POST':
+        $response = $controller->store(json_decode(file_get_contents('php://input'), true));
+        break;
+    case 'PUT':
+        $response = $controller->update($_GET['id'], json_decode(file_get_contents('php://input'), true));
+        break;
+    case 'DELETE':
+        $response = $controller->destroy($_GET['id']);
+}
+
+echo json_encode($response);
+
+function notFound() {
+    http_response_code(404);
+    exit();
+}
