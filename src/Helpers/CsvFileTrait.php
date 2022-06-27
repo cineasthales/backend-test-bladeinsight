845854b9fe9e52358f58a72e04c2150f50b4326c
Thales Cezar Castro <thalesccastro@gmail.com>
2022-06-27 12:34:05 -0300

    feat: csv file trait

---
+++ src/Helpers/CsvFileTrait.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Turbines\Helpers;
+
+trait CsvFileTrait
+{
+    public function readAllLines(string $filePath) : array
+    {
+        $lines = [];
+
+        $file = fopen($filePath . '.csv', 'r');
+
+        while (($data = fgetcsv($file)) !== false) {
+            $lines[] = $data;
+        }
+
+        fclose($file);
+
+        return $lines;
+    }
+
+    public function writeLine(string $filePath, array $newData) : bool
+    {
+        $file = fopen($filePath . '.csv', 'a');
+
+        $saved = fputcsv($file, $newData);
+
+        fclose($file);
+
+        return $saved !== false;
+    }
+
+    public function readOneLine(string $filePath, string $id) : array
+    {
+        $line = [];
+
+        $file = fopen($filePath . '.csv', 'r');
+
+        while (($data = fgetcsv($file)) !== false) {
+            if ($data[0] === $id) {
+                $line = $data;
+                break;
+            }
+        }
+
+        fclose($file);
+
+        return $line;
+    }
+
+    public function replaceLine(string $filePath, string $id, array $newData) : bool
+    {
+        $file = fopen($filePath . '.csv', 'r');
+        $fileTemp = fopen($filePath . '_temp.csv', 'w');
+
+        array_unshift($newData, $id);
+
+        $foundLine = false;
+
+        while (($data = fgetcsv($file)) !== false) {
+            if ($data[0] !== $id) {
+                fputcsv($fileTemp, $data);
+            } else {
+                fputcsv($fileTemp, $newData);
+                $foundLine = true;
+            }
+        }
+
+        fclose($file);
+        fclose($fileTemp);
+
+        $this->renameOrUnlink($foundLine, $filePath);
+
+        return $foundLine;
+    }
+
+    public function deleteLine(string $filePath, string $id) : bool
+    {
+        $file = fopen($filePath . '.csv', 'r');
+        $fileTemp = fopen($filePath . '_temp.csv', 'w');
+
+        $foundLine = false;
+
+        while (($data = fgetcsv($file)) !== false) {
+            if ($data[0] !== $id) {
+                fputcsv($fileTemp, $data);
+            } else {
+                $foundLine = true;
+            }
+        }
+
+        fclose($file);
+        fclose($fileTemp);
+
+        $this->renameOrUnlink($foundLine, $filePath);
+
+        return $foundLine;
+    }
+
+    private function renameOrUnlink(bool $foundLine, string $filePath) : void
+    {
+        if ($foundLine) {
+            rename($filePath . '_temp.csv', $filePath . '.csv');
+        } else {
+            unlink($filePath . '_temp.csv');
+        }
+    }
+}
